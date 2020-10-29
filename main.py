763476ccbc475bed04a6d19b1a30c0a64ae46b94
Aaron Wise <aaronbwise@gmail.com>
2020-10-29 10:57:10 +0700

    changed main.py kobo config

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import pandas as pd
 import json
-from dotenv import load_dotenv
 import etl.extract as extract
 import etl.transform as transform
 from etl.survey_dict import survey_dict
@@ -11,16 +10,13 @@ from etl.survey_dict import survey_dict
 config_data = json.load(open('config.json'))
 
 # Get API username and password
-load_dotenv()
-user = os.getenv('user')
-password = os.getenv('password')
-
 url = config_data['kobo']['url']
+user = config_data['kobo']['user']
+password = config_data['kobo']['password']
 
 # Survey Round and ID dictionary
 round_dict = config_data['round_dict']
 fiji_r1_id = round_dict['Fiji_R1']
-
 
 
 if __name__ == "__main__":
